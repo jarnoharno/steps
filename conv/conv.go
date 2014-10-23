e65765da1a8caeb4222a5a2c3729773ca6e1e233
Jarno Leppänen <jarno.leppanen@iki.fi>
2014-10-23 16:16:26 +0300

    Add log converter

---
+++ conv/conv.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"io"
+	"time"
+	"../server/stepsproto"
+	protoio "code.google.com/p/gogoprotobuf/io"
+)
+
+func eprint(err error) {
+	fmt.Fprintln(os.Stderr, "Error:", err)
+}
+
+func eclose(c io.Closer) {
+	err := c.Close()
+	if err != nil {
+		eprint(err)
+	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: %s input\n", os.Args[0])
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		usage()
+		os.Exit(1)
+	}
+	fn := os.Args[1]
+	if err := run(fn); err != nil {
+		eprint(err)
+		os.Exit(1)
+	}
+}
+
+func run(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer eclose(f)
+	br := bufio.NewReader(f)
+	rc := protoio.NewDelimitedReader(br, 1024)
+	defer eclose(rc)
+	return read(rc)
+}
+
+func read(r protoio.Reader) error {
+	msg := stepsproto.Message{}
+	for {
+		err := r.ReadMsg(&msg)
+		if err != nil {
+			// eof is expected
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		msgprint(&msg)
+	}
+	return nil
+}
+
+func msgprint(msg *stepsproto.Message) {
+	switch *msg.Type {
+	case stepsproto.Message_SENSOR_EVENT:
+		fmt.Printf("%s %s", ts(0, *msg.Timestamp), *msg.SensorId)
+		for _, v := range msg.Value {
+			fmt.Print(" ", v)
+		}
+		fmt.Println()
+	case stepsproto.Message_LOCATION:
+		fmt.Printf("%s %s %s %.8f %.8f %.2f %.2f %.2f %.2f\n",
+			ts(0, *msg.Timestamp),
+			*msg.SensorId,
+			ts(*msg.Utctime / 1000, *msg.Utctime % 1000),
+			*msg.Latitude,
+			*msg.Longitude,
+			*msg.Accuracy,
+			*msg.Altitude,
+			*msg.Bearing,
+			*msg.Speed)
+	}
+}
+
+func ts(sec int64, nsec int64) string {
+	//time.Unix(0, *msg.Timestamp).Format(time.RFC3339),
+	return time.Unix(sec, nsec).Format("2006-01-02 15:04:05.000")
+}
